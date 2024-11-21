63d56930b8c04ddffa235465d7a06b394fa3e638
Shayan <shayan.delbari@protonmail.com>
2024-11-21 14:37:09 -0500

    fixed a bug with the with of the line beside the experience section

--- src/routes/components/experienceCard.svelte
+++ src/routes/components/experienceCard.svelte
@@ -5,16 +5,16 @@
 	export let experience: Experience;
 </script>
 
-<div class="flex w-fit flex-row">
+<div class="relative flex w-fit flex-row">
 	<div
-		class="flex h-auto w-1 bg-black dark:bg-white
+		class="absolute flex h-full w-1 bg-black dark:bg-white
 		{index == 0 ? 'rounded-t-full' : index == length - 1 ? 'rounded-b-full' : 'rounded-none'}"
 	></div>
-	<div class="flex h-fit w-auto flex-col rounded-md p-3">
-		<div class="-mx-[24px] flex flex-row space-x-1">
-			<div
-				class="flex h-5 w-5 rounded-full border-4 border-white bg-green-600 dark:border-black"
-			></div>
+	<div class="flex h-fit w-auto flex-col p-4">
+		<div class="flex flex-row">
+			<span
+				class="absolute -start-2 flex h-5 w-5 rounded-full border-4 border-white bg-green-600 dark:border-black"
+			></span>
 			<div class="flex font-bold">{experience.company}</div>
 		</div>
 		<div
@@ -22,6 +22,6 @@
 		>
 			{experience.position}
 		</div>
-		<p class="mt-2 max-w-prose">{experience.description}</p>
+		<p class="mt-2 max-w-prose">{@html experience.description}</p>
 	</div>
 </div>
