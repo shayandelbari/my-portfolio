e2ea8deb93c9f3139fc084909c687a84f576efeb
Shayan <shayan.delbari@protonmail.com>
2024-11-22 14:00:42 -0500

    added a blur effect and made it sticky for the menu

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -9,11 +9,13 @@
 	];
 </script>
 
-<nav class="flex w-full flex-row justify-between p-5">
-	<ul class="flex"><li><a href="/"><strong>Shayan</strong> Delbari</a></li></ul>
-	<ul class="flex space-x-4">
-		{#each menuItems as item}
-			<li class="hover:text-primary"><a href={item.link}>{item.name}</a></li>
-		{/each}
-	</ul>
-</nav>
+<div class="sticky top-0 z-40 border-b-[1px] border-b-gray-900 bg-black/55 backdrop-blur">
+	<nav class="flex h-auto w-full flex-row justify-between p-5">
+		<ul class="flex"><li><a href="/"><strong>Shayan</strong> Delbari</a></li></ul>
+		<ul class="flex space-x-4">
+			{#each menuItems as item}
+				<li class="hover:text-primary"><a href={item.link}>{item.name}</a></li>
+			{/each}
+		</ul>
+	</nav>
+</div>
