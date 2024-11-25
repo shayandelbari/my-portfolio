c6eb0b6ffe0903212695ccd9816eed0cf016416e
Shayan <shayan.delbari@protonmail.com>
2024-11-25 15:09:14 -0500

    Added animation for toTop component

--- src/components/toTop.svelte
+++ src/components/toTop.svelte
@@ -14,18 +14,19 @@
 
 <svelte:window bind:scrollY={scrollHeight} />
 
-{#if showButton}
-	<button
-		on:click={gotoTop}
-		aria-label="Scroll to top"
-		class="fixed bottom-5 right-5 h-fit w-fit rounded-full bg-black p-2 shadow-lg dark:bg-white"
-		><svg
-			width="24px"
-			height="24px"
-			viewBox="0 0 24 24"
-			class="fill-none stroke-white stroke-2 dark:stroke-black"
-		>
-			<path d="M17 15L12 10L7 15" stroke-linecap="round" stroke-linejoin="round" />
-		</svg></button
+<button
+	on:click={gotoTop}
+	aria-label="Scroll to top"
+	class="fixed bottom-5 right-5 h-fit w-fit rounded-full bg-black p-2 duration-200 dark:bg-white"
+	class:translate-x-0={showButton}
+	class:translate-x-16={!showButton}
+>
+	<svg
+		width="24px"
+		height="24px"
+		viewBox="0 0 24 24"
+		class="fill-none stroke-white stroke-2 dark:stroke-black"
 	>
-{/if}
+		<path d="M17 15L12 10L7 15" stroke-linecap="round" stroke-linejoin="round" />
+	</svg>
+</button>
